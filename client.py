5ee0f940efb791c98dfa8423f5ba3c28751dddf5
Nil <Atm0n@users.noreply.github.com>
2017-09-19 14:03:45 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-# use creds to create a client to interact with the Google Drive API
-link = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', link)  # need it
-client = gspread.authorize(creds)
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("prova").sheet1
-# Extract and print all of the values
-sheet.update_cell(8, 1, "prova2")
+def update():
+    import gspread
+    from oauth2client.service_account import ServiceAccountCredentials
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    # to select what spreadsheet and sheet you want ot select(by url: cahnge it, this is an example one):
+    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1J4zBSzftrlolololololololol0o").sheet1
+    # Extract and print all of the values
+    sheet.update_cell(8, 2, "prova2")
