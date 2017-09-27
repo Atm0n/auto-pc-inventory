3167802d2ff9ddfd72426918ebf0be383fc18875
Nil <Atm0n@users.noreply.github.com>
2017-09-27 11:18:52 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
-def update():
+def update(x, y, data):
     import gspread
     from oauth2client.service_account import ServiceAccountCredentials
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
-    # to select what spreadsheet and sheet you want ot select(by url: cahnge it, this is an example one):
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1J4zBSzftrlolololololololol0o").sheet1
+    # to select what spreadsheet and sheet you want ot select(by url):
+    sheet = client.open_by_url("#############!!!##############!!!!put here your spreadsheet url").sheet1
     # Extract and print all of the values
-    sheet.update_cell(8, 2, "prova2")
+    sheet.update_cell(x, y, data)
