e87f82a16d80730389f8e70edd44cd0fe7336b75
Nil <Atm0n@users.noreply.github.com>
2017-09-29 09:47:49 +0200

    it works

--- index.py
+++ index.py
@@ -1,46 +1,50 @@
 #!/usr/bin/env python3
 import network, client  # imports from the root of the project folder
-import platform, psutil, cpuinfo, subprocess  # needed libraies
+import platform, psutil, cpuinfo, datetime  # needed libraies
 # returns the version and the name of the dist
 data = list()
+date = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
+data.append(date)
 linux_version = platform.dist()
 linux_version = linux_version[1]
 data.append(linux_version)
-
-
-print(linux_version)
 # the date of the last installed update
 linux_last_update = platform.version()
 data.append(linux_last_update)
-print(linux_last_update)
 # returns the cpu brand
 cpu = cpuinfo.get_cpu_info()
 cpu = cpu["brand"]
 data.append(cpu)
-print(cpu)
 # PC NAME
 pc_name = platform.node()
 data.append(pc_name)
-print(pc_name)
 # pc brand
 # LSWH
-
 # returns the processor achitecture
 architecture = platform.processor()
 data.append(architecture)
-print(architecture)
 # it stores into a dic all the network data
 network = network.network_inf()
-data.append(network)
-print(network)
-# disk = psutil.disk_usage("/")
-# print(disk)
+net_keys = list(network.keys())
+mac = list()
+for i in range(0, len(net_keys)):
+    key = net_keys[i]
+    if key[0:3] == "wlp":
+        network_wlan = network[key]
+        network_wlan = network_wlan["mac"]
+        mac.append(network_wlan)
+    elif key[0:3] == "enp":
+        network_eth = network[key]
+        network_eth = network_eth["mac"]
+    else:
+        pass
+# suposing that there are only 2 network interfaces, if not, you have to fix it XD
+data.append(network_wlan)  # wireless
+data.append(network_eth)    # ethernet
+
 ram = psutil.virtual_memory()  # all ram info
 ram = int(ram[0] / 1000000)
 data.append(ram)
-# only what I need :D the total amount
-print(ram)
+
 for i in range(0, len(data)):
-    print(i)
-    print(data)
-    client.update(1, i+1, str(data[i]))
+    client.update(2, i+1, str(data[i]))
