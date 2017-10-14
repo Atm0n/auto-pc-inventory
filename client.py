1a4460c0d5068991d8a0e0682f0b7338b33e28e2
Nil <Atm0n@users.noreply.github.com>
2017-10-14 16:43:02 +0200

    MYSQL version!
    all working, needed more work on it but it works!

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
-def update(x, y, data):
-    import gspread
+#!/usr/bin/env python3
+def update(data):
+    import pymysql # the used module
 
-    from oauth2client.service_account import ServiceAccountCredentials
-    # use creds to create a client to interact with the Google Drive API
-    scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    client = gspread.authorize(creds)
-    # to select what spreadsheet and sheet you want ot select(by url):
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/ put here your spreadsheet ip").sheet1
-    # Extract and print all of the values
-    sheet.update_cell(x, y, data)
+    conn = pymysql.connect(host='ip or domain', user='user!!!', passwd='passwd', db='Data base!!!') # change this
+    cur = conn.cursor()
+    sql = "INSERT INTO inventory(last_seen,version,cpu,pc_name,arch,RAM, mac_wlan, mac_eth) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)" #sql 
+    cur.execute(sql, (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])) # where the data is...
+    conn.commit() # commit changes
+    conn.close() # close connection
+
