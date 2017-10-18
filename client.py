2ff3a9eaa129f4c676e3206e12a59ef9bec2aa28
Nil <Atm0n@users.noreply.github.com>
2017-10-18 19:21:15 +0200

    minor changes
    added the posibility of update the data stored deleting the old data

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 def update(data):
-    import pymysql # the used module
+    import pymysql
 
-    conn = pymysql.connect(host='ip or domain', user='user!!!', passwd='passwd', db='Data base!!!') # change this
+    conn = pymysql.connect(host='192.168.56.102', user='user', passwd='passwd', db='db')
     cur = conn.cursor()
-    sql = "INSERT INTO inventory(last_seen,version,cpu,pc_name,arch,RAM, mac_wlan, mac_eth) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)" #sql 
-    cur.execute(sql, (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])) # where the data is...
-    conn.commit() # commit changes
-    conn.close() # close connection
-
+    sql = "DELETE FROM `inventory` WHERE `mac_wlan` = %s" # if there's an equal mac address stored, it deletes all the row
+    cur.execute(sql, (data[6])) # this executes the sql script
+    conn.commit()   # makes the changes true
+    sql = "DELETE FROM `inventory` WHERE `mac_eth` = %s" # if there's an equal mac address stored, it deletes all the row
+    cur.execute(sql, (data[7])) # this executes the sql script
+    conn.commit() # makes the changes true
+    sql = "INSERT INTO inventory(last_seen,version,cpu,pc_name,arch,RAM, mac_wlan, mac_eth) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+    cur.execute(sql, (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+    conn.commit() # makes the changes true
+    conn.close() # This closes the connection
