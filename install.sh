d8cda84a5675e78b9dbd56f72143e0fd940f431a
Nil <Atm0n@users.noreply.github.com>
2017-10-21 12:26:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 echo "welcome to the auto-pc-inventory script this will install the needed packages"
+echo "Enter the values then press enter or ctrl+c to cancel"
+echo "lets begin"
+read -p -r "halp1" mysqlip
+read -p user
+read -p secret
+read -p dbname
+echo $mysqlip >/usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
+echo $user <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
+echo $mysqlsecret <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
+echo $dbname <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
 sudo su
 mkdir /usr/local/scripts
 mkdir /usr/local/scripts/auto-pc-inventory
 chmod 755 /usr/local/scripts/auto-pc-inventory
 touch /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo "Enter the values then press enter or ctrl+c to cancel"
-echo "lets begin"
-read -p mysqlip
-read -p user
-read -p secret
-read -p dbname
-echo "$mysqlip" </usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo "$user" <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo "$mysqlsecret" <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo "$dbname" <</usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
 
 wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py -P /usr/local/scripts/auto-pc-inventory/
 apt update && sudo apt install python3 python3-pip -y
