bc274560b5e9353f7431c6eeddbe2f9c1bfd0e31
Nil <Atm0n@users.noreply.github.com>
2017-10-21 12:06:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-echo "Welcome to the auto-pc-inventory script!This will install the needed packages"
+echo "welcome to the auto-pc-inventory script this will install the needed packages"
 sudo su
 mkdir /usr/local/scripts
 mkdir /usr/local/scripts/auto-pc-inventory
 chmod 755 /usr/local/scripts/auto-pc-inventory
 touch /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo "Enter the values then press ENTER or CTRL+C to cancel"
-
+echo "Enter the values then press enter or ctrl+c to cancel"
 echo "lets begin"
 echo "ip"
 read ip
-
 echo "user"
 read -r "halp" user
 echo "secret"
@@ -21,7 +19,6 @@ echo "$mysqlip
 $user
 $mysqlsecret
 $dbname"< sudo /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-
 wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py -P /usr/local/scripts/auto-pc-inventory/
 apt update && sudo apt install python3 python3-pip -y
 pip3 install --upgrade pip
