289279cf15ae2920c963996eef05d7ae35e4439e
Nil <Atm0n@users.noreply.github.com>
2017-11-01 16:01:29 +0100

    it works
    Now i have to make something to run the script when network ready

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 echo "welcome to the auto-pc-inventory script this will install the needed packages"
 echo "Enter the values then press enter or ctrl+c to cancel"
+sleep 2
 echo "lets begin"
+sleep 1
 read mysqlip
 read user
 read secret
 read dbname
-echo $mysqlip > /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo $user >> /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo $mysqlsecret >> /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo $dbname >> /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-sudo su
-mkdir /usr/local/scripts
-mkdir /usr/local/scripts/auto-pc-inventory
-chmod 755 /usr/local/scripts/auto-pc-inventory
-touch /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-
-wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py -P /usr/local/scripts/auto-pc-inventory/
+sudo mkdir -p "/usr/local/scripts/auto-pc-inventory"
+cd "/usr/local/scripts/auto-pc-inventory"
+sudo touch auto-pc-inventory.conf
+echo $mysqlip > auto-pc-inventory.conf
+echo $user >> auto-pc-inventory.conf
+echo $mysqlsecret >> auto-pc-inventory.conf
+echo $dbname >> auto-pc-inventory.conf
+sudo wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py
 apt update && sudo apt install python3 python3-pip -y
 pip3 install --upgrade pip
 pip3 install psutil py-cpuinfo datetime pymysql
