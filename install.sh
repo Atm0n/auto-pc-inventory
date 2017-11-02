8ee959e262edf143e207fcb495736ee69897e082
Nil <Atm0n@users.noreply.github.com>
2017-11-02 21:07:48 +0100

    BIG UPDATE

--- install.sh
+++ install.sh
@@ -11,13 +11,23 @@ read secret
 echo "enter the dbname"
 read dbname
 echo "Now enter the administrator password"
-sudo su
-mkdir /usr/local/scripts
-mkdir /usr/local/scripts/auto-pc-inventory
-chmod 755 /usr/local/scripts/auto-pc-inventory
-touch /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-echo $mysqlip,$user,$secret,$dbname  > /usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf
-sudo wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py -P /usr/local/scripts/auto-pc-inventory/
-apt update && sudo apt install python3 python3-pip -y
+sed -i '/exit 0/d' /etc/rc.local
+echo "/usr/local/scripts/auto-pc-inventory/start.sh" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+mkdir -p "/usr/local/scripts/auto-pc-inventory"
+chmod 755 "/usr/local/scripts/auto-pc-inventory"
+touch "/usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf"
+echo $mysqlip,$user,$secret,$dbname  > "/usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf"
+cd "/usr/local/scripts/auto-pc-inventory"
+touch start.sh
+echo "#!/bin/sh
+sleep 60
+while ! ping -c1 $mysqlip >/dev/null
+    do echo error > /dev/null
+done
+python3 "/usr/local/scripts/auto-pc-inventory/index.py"" > start.sh
+chmod 2744 start.sh
+sudo wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py
+sudo apt update && sudo apt install python3 python3-pip -y
 pip3 install --upgrade pip
 pip3 install psutil py-cpuinfo datetime pymysql
