08c2b33af376528ad5861ace7fe62175b8f07b6c
Nil <Atm0n@users.noreply.github.com>
2017-11-17 16:56:03 +0100

    Permissions fixed
    read the title

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "welcome to the auto-pc-inventory script this will install the needed packages"
-echo "You need root permissions.Enter the values then press enter or ctrl+c to cancel"
+echo "You need root permissions.Enter the values then press enter to continue or ctrl+c to cancel"
 echo "lets begin"
 echo "enter the mysql server ip"
 read mysqlip
@@ -15,7 +15,7 @@ sed -i '/exit 0/d' /etc/rc.local
 echo "/usr/local/scripts/auto-pc-inventory/start.sh" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 mkdir -p "/usr/local/scripts/auto-pc-inventory"
-chmod 755 "/usr/local/scripts/auto-pc-inventory"
+chmod 700 "/usr/local/scripts/auto-pc-inventory"
 touch "/usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf"
 echo $mysqlip,$user,$secret,$dbname  > "/usr/local/scripts/auto-pc-inventory/auto-pc-inventory.conf"
 cd "/usr/local/scripts/auto-pc-inventory"
@@ -28,7 +28,7 @@ while ! ping -c1 $mysqlip >/dev/null
     do echo error > /dev/null
 done
 python3 "/usr/local/scripts/auto-pc-inventory/index.py"" > start.sh
-chmod 2744 start.sh
+chmod 2700 start.sh
 sudo wget https://raw.githubusercontent.com/Atm0n/auto-pc-inventory/master/index.py
 sudo apt update && sudo apt install python3 python3-pip -y
 pip3 install --upgrade pip
