851b76c3264bdc3b9188b3c1783d303ff708b167
Nil <Atm0n@users.noreply.github.com>
2017-11-21 19:14:34 +0100

    Working on it, but it works
    Still i need to add some stuff, like move the  id to the first column, put names to all the header columns AND i have to make a search box.
    And moooore things

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -1,1 +1,34 @@
-Hello world, i'm goig to be a viewer XD
+from tkinter import *
+import tkinter.ttk as ttk
+import pymysql
+
+root = Tk()
+
+
+tree = ttk.Treeview(root)
+
+conn = pymysql.connect(host=ip, user=user, passwd=passwd, db=database)
+cur = conn.cursor(pymysql.cursors.DictCursor)
+
+tree["columns"] = ("last_seen","version","cpu","pc_name","product_name","arch","RAM", "mac_wlan", "mac_eth", "oem_key")
+
+
+
+text =1047
+text2= ""
+text3=""
+
+for i in range(1, 6):
+    sql = "SELECT * FROM `inventory`"
+    cur.execute(sql)
+    conn.commit()
+    print(text)
+
+
+    for row in cur:
+       
+       tree.insert("", "end",values=(row["id"],row["last_seen"],row["version"],row["cpu"],row["pc_name"],row["product_name"],row["arch"],row["RAM"], row["mac_wlan"], row["mac_eth"], row["oem_key"]))
+       print(row)
+
+tree.pack()
+root.mainloop()
