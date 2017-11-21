8f6a7abfd7288381df28e49cec68434d6670ed29
Nil <Atm0n@users.noreply.github.com>
2017-11-21 19:19:10 +0100

    some clean
    Now, there's no duplicated entries :D

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -1,34 +1,15 @@
 from tkinter import *
 import tkinter.ttk as ttk
 import pymysql
-
 root = Tk()
-
-
 tree = ttk.Treeview(root)
-
-conn = pymysql.connect(host=ip, user=user, passwd=passwd, db=database)
+conn = pymysql.connect(ip, user, passwd, dbname)
 cur = conn.cursor(pymysql.cursors.DictCursor)
-
 tree["columns"] = ("last_seen","version","cpu","pc_name","product_name","arch","RAM", "mac_wlan", "mac_eth", "oem_key")
-
-
-
-text =1047
-text2= ""
-text3=""
-
-for i in range(1, 6):
-    sql = "SELECT * FROM `inventory`"
-    cur.execute(sql)
-    conn.commit()
-    print(text)
-
-
-    for row in cur:
-       
+sql = "SELECT * FROM `inventory`"
+cur.execute(sql)
+conn.commit()
+for row in cur:
        tree.insert("", "end",values=(row["id"],row["last_seen"],row["version"],row["cpu"],row["pc_name"],row["product_name"],row["arch"],row["RAM"], row["mac_wlan"], row["mac_eth"], row["oem_key"]))
-       print(row)
-
 tree.pack()
 root.mainloop()
