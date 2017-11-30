2e5de4a8d3a5f52d909aeddf3cb06a8402619112
Nil <Atm0n@users.noreply.github.com>
2017-11-30 14:21:49 +0100

    Update and rename index.ph to index.php

--- viewer/index.php
+++ viewer/index.php
@@ -2,6 +2,12 @@
     <head><title>Visor Inventari</title>
     <link rel="stylesheet" type="text/css" href="style.css"></head>
     <body>
+        <table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
         <?php
 require_once('config.php');
 
@@ -20,12 +26,12 @@ $row_cnt = $result->num_rows;
 if ($row_cnt > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo '<hr><h1 class="blogtitol">' . $row['titol']. '</h1><br> ' . $row['contingut']. '<br>';
+        echo '<tr><td>' . $row['id']. '</td><td> '. $row['last_seen'].'</td><td> '. $row['version'].'</td><td> '. $row['cpu'].'</td><td> '. $row['pc_name'].'</td><td> '. $row['product_name'].'</td><td> '. $row['arch'].'</td><td> '. $row['RAM'].'</td><td> '. $row['mac_wlan'].'</td><td> '. $row['mac_eth'].'</td><td> ' . $row['oem_key']. '</td></tr>';
     }
 } else {
     echo 'No hi ha entrades';
 }
-?>
+?></table>
     </body>
 
 
